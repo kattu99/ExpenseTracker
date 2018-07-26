3b5d4475d1b688514c4d06f687521103edc21b10
kattu99 <kataria.rahul172@gmail.com>
2018-07-26 18:06:18 +0530

    Added activities

--- app/src/main/java/com/example/rahulkataria/expensetracker/AddExpenseActivity.java
+++ app/src/main/java/com/example/rahulkataria/expensetracker/AddExpenseActivity.java
@@ -1,13 +1,21 @@
 package com.example.rahulkataria.expensetracker;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddExpenseActivity extends AppCompatActivity {
+
+    Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expense);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.typeArray, android.R.layout.simple_spinner_item);
     }
 }
