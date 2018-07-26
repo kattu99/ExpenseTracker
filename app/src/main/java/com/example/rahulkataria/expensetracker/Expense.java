ae090478df50a55826b9df4523540382577cad04
kattu99 <kataria.rahul172@gmail.com>
2018-07-26 18:07:06 +0530

    Added activities

--- app/src/main/java/com/example/rahulkataria/expensetracker/Expense.java
+++ app/src/main/java/com/example/rahulkataria/expensetracker/Expense.java
@@ -1,13 +1,19 @@
 package com.example.rahulkataria.expensetracker;
 
+import java.util.Date;
+
 public class Expense {
     public String location;
     public String phone_number;
     public double amount;
+    public String type;
+    public Date date;
 
-    public Expense(String location, String phone_number, double amount) {
+    public Expense(String location, String phone_number, double amount,String type,Date date) {
         this.location = location;
         this.phone_number = phone_number;
         this.amount = amount;
+        this.type = type;
+        this.date = date;
     }
 }
