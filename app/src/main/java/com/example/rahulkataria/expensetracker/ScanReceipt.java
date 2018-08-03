562975cb2ea0468d7baa6b2f2b66ddd58e80cb58
kattu99 <kataria.rahul172@gmail.com>
2018-08-03 17:38:05 +0530

    Worked on permissions and OCR detection

--- app/src/main/java/com/example/rahulkataria/expensetracker/ScanReceipt.java
+++ app/src/main/java/com/example/rahulkataria/expensetracker/ScanReceipt.java
@@ -6,17 +6,28 @@ import android.view.SurfaceView;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.vision.CameraSource;
+
 public class ScanReceipt extends AppCompatActivity {
 
     SurfaceView mCameraView;
     TextView mTextView;
     Button mButton;
+    CameraSource mCameraSource;
+
+    private static final String TAG = "ScanReceipt";
+    private static final int requestPermissionID = 101;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_receipt);
+
+        mCameraView = findViewById(R.id.surfaceView);
+        mTextView = findViewById(R.id.receipt_text);
+
+        startCameraSource();
     }
 
 
