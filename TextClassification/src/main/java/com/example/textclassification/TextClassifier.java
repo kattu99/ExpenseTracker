de95391c340d963bca1becccb6bb512dec6a56af
kattu99 <kataria.rahul172@gmail.com>
2018-08-07 23:05:18 +0530

    Made modifications to TextClassifier.java

--- TextClassification/src/main/java/com/example/textclassification/TextClassifier.java
+++ TextClassification/src/main/java/com/example/textclassification/TextClassifier.java
@@ -1,5 +1,10 @@
 package com.example.textclassification;
+import opennlp.tools.doccat.DoccatModel;
+import opennlp.tools.doccat.DocumentCategorizerME;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class TextClassifier {
 
