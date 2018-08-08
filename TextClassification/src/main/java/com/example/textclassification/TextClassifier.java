ca89682e00f7b409615e6ad2155b7fac6f9316b0
kattu99 <kataria.rahul172@gmail.com>
2018-08-08 13:59:28 +0530

    Made modifications to TextClassifier.java

--- TextClassification/src/main/java/com/example/textclassification/TextClassifier.java
+++ TextClassification/src/main/java/com/example/textclassification/TextClassifier.java
@@ -8,4 +8,30 @@ import java.io.IOException;
 
 public class TextClassifier {
 
+    DocumentCategorizerME classifier;
+
+    public TextClassifier() {
+        try {
+            classifier = DocumentCategorizer();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public DocumentCategorizerME DocumentCategorizer() throws IOException {
+
+        File trainingData = new File("Path to doccat.bin model file");
+        String classificationModelFilePath = trainingData.getAbsolutePath();
+        DocumentCategorizerME classificationME = new DocumentCategorizerME(new DoccatModel(new FileInputStream(
+                classificationModelFilePath)));
+        return classificationME;
+    }
+
+    public String predict(String text) {
+        String documentContent = text;
+        double[] classDistribution = classifier.categorize(documentContent.replaceAll("[^A-Za-z]", " ").split(" "));
+
+        return classifier.getBestCategory(classDistribution);
+    }
 }
